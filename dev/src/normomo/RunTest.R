c0d44054a2f85d4576a05147e4452e55f06dfc80
Richard White <r@rwhite.no>
2017-10-25 16:39:24 +0300

    Sykdomspuls

--- dev/src/normomo/RunTest.R
+++ dev/src/normomo/RunTest.R
@@ -15,17 +15,19 @@ a <- testthat:::JunitReporter$new()
 a$start_reporter()
 a$out <- file(file.path("/junit","normomo.xml"), "w+")
 a$start_context("normomo")
-time1 <- Sys.time()
+
 output <- processx::run("Rscript","/src/normomo/RunProcess.R", error_on_status=F)
-time2 <- Sys.time()
-duration <- difftime(time1,time2,units="secs")*1000
+cat("stdout")
+cat(output$stdout)
+cat("stderr")
+cat(output$stderr)
 
 if(output$status==0){
-  a$add_result("normomo","RunNew",testthat::expectation("success","Pass"))
+  a$add_result("normomo","RunAll",testthat::expectation("success","Pass"))
 } else {
   a$add_result("normomo","RunAll",testthat::expectation("error","Fail"))
 }
-a$add_result("normomo","Test",testthat::expectation("error","Test fail"))
+
 a$end_context("normomo")
 a$end_reporter()
 close(a$out)
--- dev/src/sykdomspuls/RunTest.R
+++ dev/src/sykdomspuls/RunTest.R
@@ -3,9 +3,6 @@ COMPUTER_NAME <- readLines(con,n=1)
 close(con)
 Sys.setenv(COMPUTER=COMPUTER_NAME)
 
-cat("##teamcity[testStarted name='Sykdomspuls.RunAll']\n")
-cat("##teamcity[testStdOut name='Sykdomspuls.RunAll' out='We hope this runs correctly']\n")
-
 # Cleaning up previous runs data
 for(baseFolder in c("/data_clean","/results","/data_app")){
   files <- list.files(file.path(baseFolder,"sykdomspuls"))
@@ -14,57 +11,53 @@ for(baseFolder in c("/data_clean","/results","/data_app")){
   }
 }
 
-cat("##teamcity[testStdOut name='Sykdomspuls.RunAll' out='Listing raw data files']\n")
-cat("##teamcity[testStdOut name='Sykdomspuls.RunAll' out='",list.files(file.path("/data_raw","sykdomspuls")),"']\n")
+unlink(file.path("/junit","sykdomspuls.xml"))
+a <- testthat:::JunitReporter$new()
+a$start_reporter()
+a$out <- file(file.path("/junit","sykdomspuls.xml"), "w+")
+a$start_context("sykdomspuls")
 
 # Run process
-time1 <- Sys.time()
+
 output <- processx::run("Rscript","/src/sykdomspuls/RunProcess.R", error_on_status=F)
-time2 <- Sys.time()
-duration <- difftime(time1,time2,units="secs")*1000
-
-cat("##teamcity[testStdOut name='Sykdomspuls.RunAll' out='",output$stdout,"']\n")
-cat("##teamcity[testStdOut name='Sykdomspuls.RunAll' out='",output$stderr,"']\n")
-
 cat("stdout")
 cat(output$stdout)
 cat("stderr")
 cat(output$stderr)
 
 if(output$status==0){
-  cat("##teamcity[testStdOut name='Sykdomspuls.RunAll' out='Looks ok']\n")
+  a$add_result("sykdomspuls","RunAll",testthat::expectation("success","Pass"))
 } else {
-  cat("##teamcity[testFailed name='Sykdomspuls.RunAll' details='FAILED']\n")
+  a$add_result("sykdomspuls","RunAll",testthat::expectation("error","Fail"))
 }
-cat("##teamcity[testFinished name='Sykdomspuls.RunAll' duration='",duration,"']\n")
 
 ## Run API
-cat("##teamcity[testStarted name='Sykdomspuls.RunAPI']\n")
-cat("##teamcity[testStdOut name='Sykdomspuls.RunAPI' out='We hope this runs correctly']\n")
-
-time1 <- Sys.time()
 process <- processx::process$new("Rscript","/src/sykdomspuls/RunAPI.R")
-if(!process$is_alive()) cat("##teamcity[testFailed name='Sykdomspuls.RunAPI' details='Died immediately']\n")
+if(process$is_alive()){
+  a$add_result("sykdomspuls","API_0min",testthat::expectation("success","Pass"))
+} else {
+  a$add_result("sykdomspuls","API_0min",testthat::expectation("error","Fail"))
+}
 Sys.sleep(120)
-if(!process$is_alive()) cat("##teamcity[testFailed name='Sykdomspuls.RunAPI' details='Server is not alive after 2 min']\n")
-
+if(process$is_alive()){
+  a$add_result("sykdomspuls","API_2min",testthat::expectation("success","Pass"))
+} else {
+  a$add_result("sykdomspuls","API_2min",testthat::expectation("error","Fail"))
+}
 
 req <- httr::GET("http://localhost:8000/test?x=0")
 json <- httr::content(req, as = "text", encoding="UTF-8")
 res <- jsonlite::fromJSON(json)
 
 if(res=="0"){
-  cat("##teamcity[testStdOut name='Sykdomspuls.RunAPI' out='Received 0 from API']\n")
+  a$add_result("sykdomspuls","API_received_0",testthat::expectation("success","Pass"))
 } else {
-  cat("##teamcity[testFailed name='Sykdomspuls.RunAPI' details='Did not receive 0 from API']\n")
+  a$add_result("sykdomspuls","API_received_0",testthat::expectation("error","Fail"))
 }
 
 x <- process$kill()
 
-time2 <- Sys.time()
-duration <- difftime(time1,time2,units="secs")*1000
-cat("##teamcity[testFinished name='Sykdomspuls.RunAPI' duration='",duration,"']\n")
-
-
-
+a$end_context("sykdomspuls")
+a$end_reporter()
+close(a$out)
 
