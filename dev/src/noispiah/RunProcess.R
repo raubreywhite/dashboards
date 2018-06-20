d96e071109b2516dcc450173f683dadb000dd1bb
Richard White <r@rwhite.no>
2018-06-20 15:16:27 +0300

    noispiah

--- dev/src/noispiah/RunProcess.R
+++ dev/src/noispiah/RunProcess.R
@@ -10,6 +10,7 @@ suppressMessages(library(ggplot2))
 
 if(Sys.getenv("RSTUDIO") == "1"){
   devtools::load_all("/packages/dashboards_noispiah/", export_all=FALSE)
+  SetConfig("FORCE_TESTING",TRUE)
 } else {
   library(noispiah)
 }
@@ -20,5 +21,15 @@ fhi::DashboardInitialise(
   SRC="src",
   NAME="noispiah"
 )
+
+CheckData()
+
+CONFIG$FILES_RMD_USE_LANDSDEKKENDE
+CONFIG$FILES_RMD_USE_FYLKE
+
+Render(dev=CONFIG$FORCE_TESTING)
+
+file.create(DashboardFolder("data_raw","DONE.txt"))
+
 
 quit(save="no")
